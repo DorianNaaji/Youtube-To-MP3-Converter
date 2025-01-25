0517152e2d5c2f869bd75a44b1e8dfb2065b9546
Dorian NAAJI <contact@dorian-naaji.fr>
2025-01-25 02:22:15 +0100

    Release 2025.1

--- YoutubeToMP3/Properties/AssemblyInfo.cs
+++ YoutubeToMP3/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dorian NAAJI")]
 [assembly: AssemblyProduct("YoutubeToMP3Form")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.1")]
-[assembly: AssemblyFileVersion("8.1")]
+[assembly: AssemblyVersion("2025.1")]
+[assembly: AssemblyFileVersion("2025.1")]
 [assembly: NeutralResourcesLanguage("en")]
 
